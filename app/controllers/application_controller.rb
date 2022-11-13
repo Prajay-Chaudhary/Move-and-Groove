710c0e0af5beb3faefbe93bd45da7b1a1840125c
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2022-11-13 15:42:04 +0100

    Changed the  redirect path after user login to activities listing path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  #Redirect to a specific page on successful sign in
+  def after_sign_in_path_for(resource)
+  activities_path # your path
+end
 end
